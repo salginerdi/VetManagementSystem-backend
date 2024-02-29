fffd071008675ceec6790f7dc456a81af877c319
Erdi Salgin <erdisalgin@yandex.com>
2024-02-29 22:22:22 +0300

    Add WebConfig

---
+++ vetsystem/src/main/java/dev/patika/vetsystem/core/config/WebConfig.java
@@ -0,0 +1,14 @@
+package dev.patika.vetsystem.core.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
