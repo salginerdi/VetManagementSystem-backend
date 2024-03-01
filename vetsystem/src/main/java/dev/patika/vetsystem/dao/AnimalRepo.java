788f5ad00524c482c85c05697a7dd0d28905bf28
Erdi Salgin <erdisalgin@yandex.com>
2024-03-01 17:35:12 +0300

    Change Method Name

--- vetsystem/src/main/java/dev/patika/vetsystem/dao/AnimalRepo.java
+++ vetsystem/src/main/java/dev/patika/vetsystem/dao/AnimalRepo.java
@@ -1,13 +1,9 @@
 package dev.patika.vetsystem.dao;
 
 import dev.patika.vetsystem.entities.Animal;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +13,7 @@ public interface AnimalRepo extends JpaRepository<Animal, Long> {
     List<Animal> findAllByCustomerId(long customerId);
 
     // 16-Hayvanları isme göre filtrelemek
-    @Query(value = "Select * From Animal Where Name ILIKE %?1%", nativeQuery = true)
-    Optional<Animal> findByName(String name);
+
+    Optional<Animal> findByNameIgnoreCaseContaining(String name);
 
 }
