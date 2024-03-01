fb188387c6818788e85c764fca0830da782416c8
Erdi Salgin <erdisalgin@yandex.com>
2024-03-01 17:45:35 +0300

    delete method

--- vetsystem/src/main/java/dev/patika/vetsystem/dao/AppointmentRepo.java
+++ vetsystem/src/main/java/dev/patika/vetsystem/dao/AppointmentRepo.java
@@ -13,19 +13,14 @@ import java.util.Optional;
 
 @Repository
 public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
-    // 22-Randevu kaydı oluştururken doktorun girilen tarihte müsait günü olup olmadığı, eğer varsa randevu
-    // kayıtlarında girilen saatte başka bir randevusu olup olmadığı kontrol edilir.
-    @Query(value = "SELECT * FROM appointment WHERE appointment_date = ?1", nativeQuery = true)
-    Optional<Appointment> checkForConflictingAppointmentHours(LocalDateTime appointment);
-
     // 24-Randevuların girilen tarih aralığı ve doktora göre filtrelenmesi
-    List<Appointment> findByAppointmentDateAndDoctor(LocalDateTime appointmentDate, Doctor doctor);
+    List<Appointment> findByAppointmentDateAndDoctorId(LocalDateTime appointmentDate, Long doctorId);
 
     // 23-Randevuların girilen tarih aralığı ve hayvana göre filtrelenmesi
-    List<Appointment> findByAppointmentDateAndAnimal(LocalDateTime appointmentDate, Animal animal);
+    List<Appointment> findByAppointmentDateAndAnimalId(LocalDateTime appointmentDate, Long animalId);
 
     // 22-Hayvanların her türlü muayenesi için doktorlardan uygun tarihlerde ve saatlerde randevu oluşturulmalıdır.
     // Her doktor için sadece saat başı randevu oluşturulması.
     @Query(value = "SELECT * FROM appointment WHERE appointment_date = ?1 AND doctor_id = ?2", nativeQuery = true)
-    Optional<Appointment> checkForConflictingAppointmentHoursByDoctorAndHour(LocalDateTime appointment, Long doctorId);
+    Optional<Appointment> checkForConflictingAppointmentHoursByHourAndDoctorId(LocalDateTime appointment, Long doctorId);
 }
