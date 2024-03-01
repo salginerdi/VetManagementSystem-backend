1c1a8034aa3680e73ec411582ee3042ac3f73867
Erdi Salgin <erdisalgin@yandex.com>
2024-03-01 17:40:34 +0300

    delete file

--- vetsystem/src/main/java/dev/patika/vetsystem/core/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package dev.patika.vetsystem.core.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
