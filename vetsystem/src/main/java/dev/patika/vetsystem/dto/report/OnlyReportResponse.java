aebae15ca5ede9da18fb9607dda330708b67fbdd
Erdi Salgin <erdisalgin@yandex.com>
2024-03-01 17:55:42 +0300

    add Report Response

--- vetsystem/src/main/java/dev/patika/vetsystem/dto/report/OnlyReportResponse.java
+++ vetsystem/src/main/java/dev/patika/vetsystem/dto/report/OnlyReportResponse.java
@@ -1,4 +1,14 @@
 package dev.patika.vetsystem.dto.report;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OnlyReportResponse {
+    private Long id;
+    private String title;
+    private String diagnosis;
+    private double price;
 }
--- vetsystem/src/main/java/dev/patika/vetsystem/dto/report/ReportResponse.java
+++ vetsystem/src/main/java/dev/patika/vetsystem/dto/report/ReportResponse.java
@@ -1,4 +1,10 @@
 package dev.patika.vetsystem.dto.report;
 
 public class ReportResponse {
+    private Long id;
+    private String title;
+    private String diagnosis;
+    private double price;
+    private OnlyAppointmentResponse appointment;
+    private List<OnlyVaccineResponse> vaccines;
 }
