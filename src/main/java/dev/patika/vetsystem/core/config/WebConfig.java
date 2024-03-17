ecc55554c6eb8f081e5d2cecd47b854219883337
Erdi Salgin <erdisalgin@yandex.com>
2024-03-17 23:12:09 +0300

    changed web config

--- src/main/java/dev/patika/vetsystem/core/config/WebConfig.java
+++ src/main/java/dev/patika/vetsystem/core/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://vetsystem.netlify.app/")
+                .allowedOrigins("https://vetsystem.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
